7126293010f47273d38e1219407f77bfc2362058
Wisner Marques <wisner.a.marques@gmail.com>
2024-01-17 09:52:45 -0300

    atualizado

--- db/conn.js
+++ db/conn.js
@@ -2,7 +2,7 @@ const knex = require('knex')
 const configuration = require('../knexfile')
 
 // Na constante defina qual ambiente vai utilizar: 'development' ou 'production'
-const environment = 'production'
+const environment = process.env.NODE_ENV || 'production'
 
 // Usa a configuração do ambiente correspondente
 const conn = knex(configuration[environment])
